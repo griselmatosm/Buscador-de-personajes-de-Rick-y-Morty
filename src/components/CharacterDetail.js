45c1a2a8363de0b84dab6dc51f5de9b6421a0411
Grisel Matos <griselmatosm@gmail.com>
2020-04-23 02:56:52 +0200

    Add styles in character detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/CharacterDetail.css';
 
-function ChatacterDetail(props) {
+function CharacterDetail(props) {
   console.log(props);
 
   return (
-    <div className="ChatacterDetail">
-      <Link to="/"> Volver </Link>
-      <img src={props.character.image} alt={`Foto de ${props.character.name}`} />
-      <h3>{props.character.name}</h3>
-      <p>Status: {props.character.status}</p>
-      <p>Species: {props.character.species}</p>
-      <p>Origin: {props.character.planet}</p>
-      <p>Episodes: {props.character.episodes}</p>
+    <div className="CharacterDetail">
+      <Link className="Character-link" to="/"> Volver </Link>
+      <div className="Character-info">
+        <img className="Character-image" src={props.character.image} alt={`Foto de ${props.character.name}`} />
+        <div>
+          <h3>{props.character.name}</h3>
+          <p>Status: {props.character.status}</p>
+          <p>Species: {props.character.species}</p>
+          <p>Origin: {props.character.planet}</p>
+          <p>Episodes: {props.character.episodes}</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default ChatacterDetail;
+export default CharacterDetail;
